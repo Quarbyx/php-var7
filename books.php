6c7bd779c816e4904aaba3ca2fa4f0d2051d4d0d
Рома <160597335+Quarbyx@users.noreply.github.com>
2025-11-11 09:19:01 +0400

    lr6upd

--- books.php
+++ books.php
@@ -1,15 +1,37 @@
-<?xml version="1.0" encoding="utf-8"?> 
-<catalog>
-<book>
-		<author>Дмитрий Котеров, Игорь Симдянов</author>
-		<title>PHP 7</title>
-		<pubyear>2016</pubyear>
-<price>600</price>
-</book>
-<book>
-<author>Люк Веллинг, Лора Томсон</author>
-		<title>Разработка веб-приложений с помощью PHP и MySQL</title>
-		<pubyear>2010</pubyear>
-<price>300</price>
-</book>
-</catalog>
+<?php
+header("Content-type: text/html; charset=UTF-8");
+
+// Загружаем XML-файл
+$sxml = simplexml_load_file("books.xml");
+?>
+
+<html>
+<head>
+<title>Каталог</title>
+</head>
+<body>
+<h1>Каталог книг</h1>
+
+<table border="1" width="100%">
+<tr>
+    <th>Автор</th>
+    <th>Название</th>
+    <th>Год издания</th>
+    <th>Цена, руб</th>
+</tr>
+
+<?php
+foreach ($sxml->book as $book) {
+    echo "<tr>";
+    echo "<td>" . $book->author . "</td>";
+    echo "<td>" . $book->title . "</td>";
+    echo "<td>" . $book->pubyear . "</td>";
+    echo "<td>" . $book->price . "</td>";
+    echo "</tr>";
+}
+?>
+
+</table>
+
+</body>
+</html>
